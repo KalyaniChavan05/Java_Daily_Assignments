fbea2ed86140abf3f6158b6b7b6c5c946f06be71
Kalyani <kalyanichavan618@gmail.com>
2024-08-26 23:32:10 +0530

    Day 1 Assignments

---
+++ Day1/Assign2.java
@@ -0,0 +1,15 @@
+class Assign2
+{
+	public static void main(String args[])
+	{
+		int n=5;
+		if ( n % 2 ==1)
+		{
+			System.out.println(" Number is Odd ");
+		}
+		else
+		{
+			System.out.println(" Number is Even ");
+		}
+	}
+}
