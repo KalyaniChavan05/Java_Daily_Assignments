88714e17c8aed77737f02b38cc5e059b53be4230
Kalyani <kalyanichavan618@gmail.com>
2024-08-29 22:42:03 +0530

    Day 4 Assignments

---
+++ Day4/Assign1.java
@@ -0,0 +1,13 @@
+class Assign1
+{
+	public static void main(String args[])
+	{
+		for( char i='A';  i<='I'; i++)
+		{
+			if( i % 2 ==1)
+			{
+				System.out.print(i+ " ");
+			}
+		}
+	}
+}
---
+++ Day4/Assign3.java
@@ -0,0 +1,14 @@
+class Assign3
+{
+	public static void main(String args[])
+	{
+		for( int i=0 ; i<=2; i++)
+		{
+			for(int j=1; j<=4; j++)
+			{
+				System.out.print("*"+" ");			
+			}
+			System.out.println();
+		}
+	}
+}
