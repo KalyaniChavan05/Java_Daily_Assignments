0cb56da18b636d279d2b229ad6ce49c4795d2da6
Kalyani <kalyanichavan618@gmail.com>
2024-09-04 22:49:12 +0530

    Day assignment updated

--- Day6/Assign8.java
+++ Day6/Assign8.java
@@ -1,19 +1,17 @@
-class Assign8
-{
-	public static void main(String args[])
-	{
-		int n=1;
-		for(int i=1;i<=3;i++)
-		{
-			for(int j=1; j<=4;j++)
-			{
-				if(n % 2 ==1)
-				{
-					System.out.print(n+" ");
-					n++;
-				}
-			}
-			System.out.println();
-		}
-	}
+class Assign8 {
+    public static void main(String[] args) {
+        int rows = 3; // Number of rows
+        int columns = 4; // Number of columns
+        int startNum = 1; // Starting number
+
+        for (int i = 0; i < rows; i++) {
+            int currentNum = startNum;
+            for (int j = 0; j < columns; j++) {
+                System.out.print(currentNum + " ");
+                currentNum += 2;
+            }
+            System.out.println();
+            startNum += 2; // Increment the starting number for the next row
+        }
+    }
 }
