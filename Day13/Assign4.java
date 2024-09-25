be82ab51da72f5914cc1e5b0868f945efeacd7b1
Kalyani <kalyanichavan618@gmail.com>
2024-09-25 23:10:02 +0530

    Day 13 Assignments

---
+++ Day13/Assign4.java
@@ -0,0 +1,13 @@
+class Assign4
+{
+	public static void main(String args[])
+	{
+		int n=5;
+		int facto=1 ;
+		for(int i=1; i<=n; i++)
+		{
+			facto= facto*i;
+		}
+			System.out.println(facto);
+	}
+}
